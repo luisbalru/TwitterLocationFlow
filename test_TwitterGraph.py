9bdedd8c06000c64e3de95fb8351646ebb1d8dac
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-10-12 12:30:04 +0200

    Añadiendo los tests #6

---
+++ test_TwitterGraph.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+import unittest
+
+from TwitterGraph import TwitterGraph
+
+class TestTwitterGraph(unittest.TestCase):
+
+    def setUp(self):
+        self.__graph_dict = TwitterGraph()
+
+    def test_should_initialize_object_OK(self):
+        self.assertIsInstance(self.__graph_dict,TwitterGraph, "Objeto creado correctamente")
+
+    def test_should_have_graph_stored_correctly( self):
+        self.assertIsInstance( self.__graph_dict.grafo(), dict, "El objeto graph_dict contiene un diccionario")
+
+    def test_add_vertice_correctamente(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.add_vertice(1)
+
+    def test_add_arista_correctamente(self):
+        with self.assertRaises(ValueError):
+            self.__graph_dict.add_arista([1,2])
+
+    def test_remove_vertice(self):
+        with self.assertRaises(ValueError):
+            self.__graph_dict.elimina_vertice(1)
+
+    def test_remove_arista(self):
+        with self.assertRaises(ValueError):
+            self.__graph_dict.elimina_conexion(1,2)
+
+    def test_grado_vertice(self):
+        with self.assertRaises(KeyError):
+            self.__graph_dict.grado_vertice(1)
+
+    
