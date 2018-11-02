9d9c9f3f272b89bb6b642dba7a496717390a00b1
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-11-02 17:32:49 +0100

    Indentation errors

--- tests/test_TG_rest_api.py
+++ tests/test_TG_rest_api.py
@@ -11,11 +11,11 @@ class testAPI(unittest.TestCase):
 	def test_root(self):
 		response = requests.get(url)
 		self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
-        self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
+        	self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
 
 	def test_status(self):
 		response = requests.get(url + '/status')
-        self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
+        	self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
 		self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
 
 	
