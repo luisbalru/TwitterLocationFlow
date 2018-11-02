ab2b6e7d25c6aad6535abba9da6b23eff28e5254
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-11-02 13:24:56 +0100

    Escribiendo tests #14

---
+++ tests/test_TG_rest_api.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import unittest, json, requests
+from requests import *
+
+# Completar
+url = 'https://'
+
+assert context
+
+class testAPI(unittest.TestCase):
+	def test_root(self):
+		response = requests.get(url)
+		self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
+        self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
+
+	def test_status(self):
+		response = requests.get(url + '/status')
+        self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
+		self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
+
+	
