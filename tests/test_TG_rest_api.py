bfd2a9151504d4f644eddab3942a308e29d10907
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-11-03 16:45:37 +0100

    Actualizando tests #14

--- tests/test_TG_rest_api.py
+++ tests/test_TG_rest_api.py
@@ -3,23 +3,18 @@ import unittest, json, requests
 from requests import *
 
 # Completar
-url = 'https://iv1819-twitterlocationflow.herokuapp.com/'
-
-assert context
+url = 'https://iv1819-twitterlocationflow.herokuapp.com'
 
 class testAPI(unittest.TestCase):
 	def test_root(self):
 		response = requests.get(url)
-		self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
-        self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
+		self.assertEqual(response.status_code,200, "Código correcto")
+		self.assertEqual(response.json().['status'],'OK',"Estado correcto")
 
 	def test_status(self):
-		response = requests.get(url + '/status')
-        self.assertEqual(response.status_code, 200, "Devuelve codigo correcto.")
-		self.assertEqual(response.json()['status'], 'OK', "Devuelve estado correcto.")
-
-
-
+		response = requests.get(url+'/status')
+		self.assertEqual(response.status_code, 200, "Código correcto")
+		self.assertEqual(response.json()['status'], 'OK', "Estado correcto")
 
 
 if __name__ == '__main__':
