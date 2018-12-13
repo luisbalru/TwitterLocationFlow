32fc26eebae521963f9e975c2f908f7037cc2566
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-13 22:13:30 +0100

    Close #29

--- src/TwitterGraph.py
+++ src/TwitterGraph.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import json
 import os
+from gremlin_python.driver import client, serializer
+import sys, traceback
 
 
 class TwitterGraph:
@@ -11,49 +13,31 @@ class TwitterGraph:
 
     def __init__(self):
         """ Inicializa el objeto grafo """
-        try: # Basado en el HitosIV.py de JJ/tests-python
-            if os.path.isfile('nodes.json'):
-                path = 'nodes.json'
-            elif os.path.isfile('/data/nodes.json'):
-                path = '/data/nodes.json'
-            elif os.path.isfile('./data/nodes.json'):
-                path = './data/nodes.json'
-            elif os.path.isfile('../data/nodes.json'):
-                path = '../data/nodes.json'
-            else:
-                raise IOError("No se encuentra 'nodes.json'")
 
-            with open(path,"r") as data_file:
-                self.__graph_dict = json.loads(data_file.read())
-        except IOError as fallo:
-            print("Error {:s} leyendo el fichero nodes.json".format(fallo))
 
-    def vertices(self):
+    def vertices(client):
         """ Devuelve los vértices del grafo """
-        return list(self.__graph_dict.keys())
+        query = "g.V().hasLabel('vertex')"
+        callback = client.submitAsync(query)
+        resultado = ""
+        for result in callback.result():
+            resultado = resultado + "\t{0}".format(str(result))
+        return resultado
 
-    def aristas(self):
-        """ Devuelve las aristas del grafo """
-        return self.__generar_aristas()
-
-    def grafo(self):
-        """ Devuelve el dictionario donde se almacena el grafo"""
-        return self.__graph_dict
-
-    def add_vertice(self, vertex):
-        """ Si el vértice 'vertex' no está en self.__graph_dict
-            se introduce en el diccionario una nueva clave
-            'vertex' con una lista vacía como valor.
-            En otro caso, no se hace nada
+    def add_vertice(client, vertex):
+        """
+            Añade un vértice con id 'vertex' nuevo al grafo.
+            El vértice debe ser un string
         """
         if (not type(vertex) is str):
             raise TypeError("El vértice debe ser un string")
-        if vertex in self.__graph_dict:
-            raise ValueError("Este vértice ya existe")
 
-        self.__graph_dict[vertex] = []
+        query = "g.addV('vertex').property('id','" + str + "').property('name','"+str+"')"
+        callback = client.submitAsync(query)
+        if callback.result() is None:
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-    def add_arista(self, arista):
+    def add_arista(client, arista):
         """ arista es un conjunto, tupla o lista """
         arista = set(arista)
         vertice1 = arista.pop()
@@ -62,125 +46,38 @@ class TwitterGraph:
         else:
             vertice2 = vertice1
 
-        if(not (vertice1 in self.__graph_dict) or not(vertice2 in self.__graph_dict)):
-            raise ValueError("Los vértices dados no pertenecen al grafo")
+        query = "g.V('" + vertice1 + "').addE('knows').to(g.V('"+ vertice2 +"'))"
+        callback = client.submitAsync(query)
+        if callback.result() is None:
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-        elif vertice1 in self.__graph_dict and vertice2 in self.__graph_dict:
-            self.__graph_dict[vertice1].append(vertice2)
-            self.__graph_dict[vertice2].append(vertice1)
-
-    def elimina_conexion(self, vertice1, vertice2):
+    def elimina_conexion(client, vertice1, vertice2):
         """ Elimina la arista entre vertice1 y vertice2 """
-        if vertice1 in self.__graph_dict:
-            aristas = self.__graph_dict[vertice1]
-        else:
-            raise ValueError("El primer vértice no está en el grafo")
-        if aristas:
-            if vertice2 in aristas:
-                aristas.remove(vertice2)
-                self.__graph_dict[vertice1] = aristas
-            else:
-                raise ValueError("Los vértices no están conectados")
-        else:
-            raise ValueError("Los vértices no están conectados")
+        query = "g.V('" + vertice1 + "').outE('knows').where(inV().has('vertex','"+vertice2+"')).drop()"
+        callback = client.submitAsync(query)
+        if callback.result() is None:
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-        if vertice2 in self.__graph_dict:
-            aristas = self.__graph_dict[vertice2]
-        else:
-            raise ValueError("El segundo vértice no está en el grafo")
-
-        if aristas:
-            if vertice1 in aristas:
-                aristas.remove(vertice1)
-                self.__graph_dict[vertice2] = aristas
-            else:
-                raise ValueError("Los vértices no están conectados")
-
-    def elimina_vertice(self, vertice):
+    def elimina_vertice(client, vertice):
         """ Elimina un vértice del grafo (y todas sus conexiones) """
-        if vertice in self.__graph_dict:
-            del self.__graph_dict[vertice]
-            for vertex in self.__graph_dict:
-                if vertice in self.__graph_dict[vertex]:
-                    self.__graph_dict[vertex].remove(vertice)
+        query = "g.V('"+vertice+"').drop()"
+        callback = client.submitAsync(query)
+        if callback.result() is None:
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
+
+    def cuenta_vertices(client):
+        query = "g.V().count()"
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            return ("\t{0}".format(callback.result().one()))
         else:
-            raise ValueError("No existe el vértice")
-
-    def __generar_aristas(self):
-        """ Método estático que genera las aristas del grafo
-            'graph'. Las aristas se representan como conjuntos
-            con uno o dos vértices
-        """
-        aristas = []
-        for vertice in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertice]:
-                if {neighbour,vertice} not in aristas:
-                    aristas.append(({neighbour,vertice}))
-        return aristas
-
-    def __str__(self):
-        res = "vertices: "
-        for k in self.__graph_dict:
-            res += str(k) + " "
-        res += "\nedges: "
-        for edge in self.__generar_aristas():
-            res += str(edge) + " "
-        return res
-
-    def vertices_aislados(self):
-        """ devuelve una lista de vertices aislados. """
-        graph = self.__graph_dict
-        isolated = []
-        for vert in graph:
-            if not graph[vert]:
-                isolated += [vert]
-        return isolated
-
-    def grado_vertice(self, vertice):
-        """ El grado de un vértice es el número de
-            aristas que conectan con él, es decir,
-            el número de vértices adyacentes.
-        """
-        if vertice in self.__graph_dict:
-            adj_vertices = self.__graph_dict[vertice]
-        else:
-            raise KeyError("El vértice no está en el grafo")
-
-        grado = len(adj_vertices)
-        return grado
-
-    def find_path(self, start_vertex, end_vertex, path=[]):
-        """ Función recuersiva que encuentra un camino entre un vértice de
-            partida y otro de llegada
-        """
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex, end_vertex, path)
-                if extended_path:
-                    return extended_path
-        return None
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
 
-    def find_all_paths(self, start_vertex, end_vertex, path=[]):
-        """ Encuentra todos los caminos entre un vértices
-            de partida y un vértice de llegada
-        """
-        graph = self.__graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return [path]
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths(vertex, end_vertex,path)
-                for p in extended_paths:
-                    paths.append(p)
-        return paths
+    def find_path(client, start_vertex, end_vertex):
+        query = "g.V('"+start_vertex+"').repeat(out()).util(has('id','"+end_vertex+"')).path().by('name')"
+        callback = client.submitAsync(query)
+        resultado = ""
+        for result in callback.result():
+            resultado = resultado + "\t{0}".format(str(result))
+        return resultado
--- tests/test_TwitterGraph.py
+++ tests/test_TwitterGraph.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 import unittest
-
+from gremlin_python.driver import client, serializer
 from src.TwitterGraph import TwitterGraph
 
 class TestTwitterGraph(unittest.TestCase):
@@ -9,56 +9,37 @@ class TestTwitterGraph(unittest.TestCase):
 
     def setUp(self):
         self.__graph_dict = TwitterGraph()
-
+        try:
+            client = client.Client('wss://twitterlocationflow.gremlin.cosmosdb.azure.com:443/','g',username="/dbs/db-iv-ugr/colls/grafo-iv-ugr",password="2r4SoDdlxiXaqxsLgb6yohACz6tVWuv5dgbQ3dqQ5rvvZInn3cUW7pNFHg8DeW5gxSj3xi1KSG3X95qaqBNZjA")
+        except TypeError:
+            print("Hubo un error!")
+            
     def test_should_initialize_object_OK(self):
         self.assertIsInstance(self.__graph_dict,TwitterGraph, "Objeto creado correctamente")
 
-    def test_should_have_graph_stored_correctly( self):
-        self.assertIsInstance( self.__graph_dict.grafo(), dict, "El objeto graph_dict contiene un diccionario")
-
     def test_add_vertice_correctamente(self):
         with self.assertRaises(TypeError):
-            self.__graph_dict.add_vertice(1)
+            self.__graph_dict.add_vertice(client,1)
 
     def test_add_arista_correctamente(self):
         with self.assertRaises(ValueError):
-            self.__graph_dict.add_arista([1,2])
+            self.__graph_dict.add_arista(client,[1,2])
 
     def test_remove_vertice(self):
         with self.assertRaises(ValueError):
-            self.__graph_dict.elimina_vertice(1)
+            self.__graph_dict.elimina_vertice(client,1)
 
     def test_remove_arista(self):
         with self.assertRaises(ValueError):
-            self.__graph_dict.elimina_conexion(1,2)
-
-    def test_grado_vertice(self):
-        with self.assertRaises(KeyError):
-            self.__graph_dict.grado_vertice(1)
+            self.__graph_dict.elimina_conexion(client,1,2)
 
     def test_find_path(self):
-        self.__graph_dict.add_vertice('n')
-        self.__graph_dict.add_vertice('m')
-        self.__graph_dict.add_vertice('t')
-        self.__graph_dict.add_arista(['n','m'])
-        self.__graph_dict.add_arista(['m','t'])
-        self.assertEqual(self.__graph_dict.find_path('n','t'), ['n', 'm', 't'], "Camino correcto")
-
-    def test_find_all_paths(self):
-        self.__graph_dict.add_vertice('n')
-        self.__graph_dict.add_vertice('m')
-        self.__graph_dict.add_vertice('aux')
-        self.__graph_dict.add_vertice('t')
-        self.__graph_dict.add_arista(['n','m'])
-        self.__graph_dict.add_arista(['n','aux'])
-        self.__graph_dict.add_arista(['aux','t'])
-        self.__graph_dict.add_arista(['m','t'])
-        self.assertEqual(self.__graph_dict.find_all_paths('n','t'),[['n', 'm', 't'], ['n', 'aux', 't']], "Caminos correctos")
-
-    def test_isolated_vertex(self):
-        self.__graph_dict.add_vertice('n')
-        self.assertEqual(self.__graph_dict.vertices_aislados(),['n'], "El único vértice aislado es 'n'")
-
+        self.__graph_dict.add_vertice(client,'n')
+        self.__graph_dict.add_vertice(client,'m')
+        self.__graph_dict.add_vertice(client,'t')
+        self.__graph_dict.add_arista(client,['n','m'])
+        self.__graph_dict.add_arista(client,['m','t'])
+        self.assertEqual(self.__graph_dict.find_path(client,'n','t'), ['n', 'm', 't'], "Camino correcto")
 
 
 if __name__ == '__main__':
