35d08f296ac05297bb9c1159dc9494f767c1a976
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-14 10:36:35 +0100

    Seguimos con los tests #32

--- src/TwitterGraph.py
+++ src/TwitterGraph.py
@@ -11,11 +11,7 @@ class TwitterGraph:
         Añadidas algunas modificaciones.
     """
 
-    def __init__(self):
-        """ Inicializa el objeto grafo """
-
-
-    def vertices(client):
+    def vertices(self,client):
         """ Devuelve los vértices del grafo """
         query = "g.V().hasLabel('vertex')"
         callback = client.submitAsync(query)
@@ -24,7 +20,7 @@ class TwitterGraph:
             resultado = resultado + "\t{0}".format(str(result))
         return resultado
 
-    def add_vertice(client, vertex):
+    def add_vertice(self,client, vertex):
         """
             Añade un vértice con id 'vertex' nuevo al grafo.
             El vértice debe ser un string
@@ -32,12 +28,12 @@ class TwitterGraph:
         if (not type(vertex) is str):
             raise TypeError("El vértice debe ser un string")
 
-        query = "g.addV('vertex').property('id','" + str + "').property('name','"+str+"')"
+        query = "g.addV('vertex').property('id','" + vertex + "').property('name','"+vertex+"')"
         callback = client.submitAsync(query)
         if callback.result() is None:
             raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-    def add_arista(client, arista):
+    def add_arista(self,client, arista):
         """ arista es un conjunto, tupla o lista """
         arista = set(arista)
         vertice1 = arista.pop()
@@ -45,27 +41,33 @@ class TwitterGraph:
             vertice2 = arista.pop()
         else:
             vertice2 = vertice1
+        if(not type(vertice1) is str):
+            raise TypeError("El vértice debe ser un string")
 
-        query = "g.V('" + vertice1 + "').addE('knows').to(g.V('"+ vertice2 +"'))"
+        query = "g.V('" + str(vertice1) + "').addE('knows').to(g.V('"+ str(vertice2) +"'))"
         callback = client.submitAsync(query)
         if callback.result() is None:
             raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-    def elimina_conexion(client, vertice1, vertice2):
+    def elimina_conexion(self,client, vertice1, vertice2):
         """ Elimina la arista entre vertice1 y vertice2 """
-        query = "g.V('" + vertice1 + "').outE('knows').where(inV().has('vertex','"+vertice2+"')).drop()"
+        if(not type(vertice1) is str or not type(vertice2) is str):
+            raise TypeError("Los vértices deben ser strings")
+        query = "g.V('" + str(vertice1) + "').outE('knows').where(inV().has('vertex','"+str(vertice2)+"')).drop()"
         callback = client.submitAsync(query)
         if callback.result() is None:
             raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-    def elimina_vertice(client, vertice):
+    def elimina_vertice(self,client, vertice):
         """ Elimina un vértice del grafo (y todas sus conexiones) """
-        query = "g.V('"+vertice+"').drop()"
+        if(not type(vertice) is str):
+            raise TypeError("El vértice debe ser string")
+        query = "g.V('"+str(vertice)+"').drop()"
         callback = client.submitAsync(query)
         if callback.result() is None:
             raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
-    def cuenta_vertices(client):
+    def cuenta_vertices(self,client):
         query = "g.V().count()"
         callback = client.submitAsync(query)
         if callback.result() is not None:
@@ -74,10 +76,15 @@ class TwitterGraph:
             raise ValueError("Algo fue mal con esta query:{0}".format(query))
 
 
-    def find_path(client, start_vertex, end_vertex):
-        query = "g.V('"+start_vertex+"').repeat(out()).util(has('id','"+end_vertex+"')).path().by('name')"
+    def find_path(self,client, start_vertex, end_vertex):
+        if(not type(start_vertex) is str):
+            raise TypeError("El vértice debe ser string")
+        query = "g.V('"+str(start_vertex)+"').repeat(out()).until(has('id','"+str(end_vertex)+"')).path().by('name')"
         callback = client.submitAsync(query)
         resultado = ""
-        for result in callback.result():
-            resultado = resultado + "\t{0}".format(str(result))
-        return resultado
+        if callback.result() is not None:
+            for result in callback.result():
+                resultado = resultado + "\t{0}".format(str(result))
+            return resultado
+        else:
+            raise ValueError("Algo fue mal con esta query:{0}".format(query))
---
+++ src/test_TwitterGraph.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+import unittest
+from gremlin_python.driver import client, serializer
+from TwitterGraph import TwitterGraph
+
+class TestTwitterGraph(unittest.TestCase):
+    """ Clase creada para pasar tests a TwitterGraph """
+
+    def setUp(self):
+        self.__graph_dict = TwitterGraph()
+        try:
+            self.cliente = client.Client('wss://twitterlocationflow.gremlin.cosmosdb.azure.com:443/','g',
+                        username="/dbs/db-iv-ugr/colls/grafo-iv-ugr",
+                        password="2r4SoDdlxiXaqxsLgb6yohACz6tVWuv5dgbQ3dqQ5rvvZInn3cUW7pNFHg8DeW5gxSj3xi1KSG3X95qaqBNZjA==",
+                        message_serializer=serializer.GraphSONSerializersV2d0())
+        except Exception as e:
+            print('There was an exception: {0}'.format(e))
+            traceback.print_exc(file=sys.stdout)
+            sys.exit(1)
+
+    def test_add_vertice_correctamente(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.add_vertice(self.cliente,1)
+
+    def test_add_arista_correctamente(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.add_arista(self.cliente,[1,2])
+
+    def test_remove_vertice(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.elimina_vertice(self.cliente,1)
+
+    def test_remove_arista(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.elimina_conexion(self.cliente,1,2)
+
+    def test_find_path(self):
+        with self.assertRaises(TypeError):
+            self.__graph_dict.find_path(self.cliente,1,2)
+
+
+if __name__ == '__main__':
+    unittest.main()
