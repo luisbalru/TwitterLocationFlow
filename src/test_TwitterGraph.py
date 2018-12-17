e257364bbd0094b4789fe3faaad5940a9ee2074b
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-17 12:31:12 +0100

    Modificación

--- src/test_TwitterGraph.py
+++ src/test_TwitterGraph.py
@@ -9,15 +9,7 @@ class TestTwitterGraph(unittest.TestCase):
 
     def setUp(self):
         self.__graph_dict = TwitterGraph()
-        try:
-            self.cliente = client.Client('wss://twitterlocationflow.gremlin.cosmosdb.azure.com:443/','g',
-                        username="/dbs/db-iv-ugr/colls/grafo-iv-ugr",
-                        password="2r4SoDdlxiXaqxsLgb6yohACz6tVWuv5dgbQ3dqQ5rvvZInn3cUW7pNFHg8DeW5gxSj3xi1KSG3X95qaqBNZjA==",
-                        message_serializer=serializer.GraphSONSerializersV2d0())
-        except Exception as e:
-            print('There was an exception: {0}'.format(e))
-            traceback.print_exc(file=sys.stdout)
-            sys.exit(1)
+        
 
     def test_add_vertice_correctamente(self):
         with self.assertRaises(TypeError):
