d1ca9419ffe9811e7a39fd48bd5211e8fb12f1ad
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-19 12:31:02 +0100

    Añadiendo app #33

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import json, os
 from flask import Flask, jsonify, request, make_response
 from src.TwitterGraph import TwitterGraph
 
+try:
+        self.cliente = client.Client(os.environ['C_URL'],'g',
+                        username=os.environ['C_USER'],
+                        password=os.environ['C_PASS'],
+                        message_serializer=serializer.GraphSONSerializersV2d0())
+except Exception as e:
+        print('There was an exception: {0}'.format(e))
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(1)
 
 app = Flask(__name__)
 
@@ -22,23 +31,27 @@ def status():
 
 @app.route("/vertices", methods=['POST'])
 def vertices():
-    return jsonify(success=TwitterGraph.vertices())
+    return jsonify(success=TwitterGraph.vertices(cliente))
 
 @app.route("/vertices/add/<vertex>", methods=['POST'])
 def add_vertex(vertex):
-    return jsonify(success=TwitterGraph.add_vertice(str(vertex)))
+    return jsonify(success=TwitterGraph.add_vertice(cliente,str(vertex)))
 
 @app.route("/aristas/add/<edge>", methods=['POST'])
 def add_edge(edge):
-    return jsonify(success=TwitterGraph.add_vertice(str(edge)))
+    return jsonify(success=TwitterGraph.add_vertice(cliente,str(edge)))
 
 @app.route("/vertices/remove/<vertex>", methods=['DELETE'])
 def remove_vertex(vertex):
-    return jsonify(success=TwitterGraph.elimina_vertice(str(vertex)))
+    return jsonify(success=TwitterGraph.elimina_vertice(cliente,str(vertex)))
 
-@app.route("/aristas/remove/<edge>", methods=['DELETE'])
+@app.route("/aristas/remove/<edge1>/<edge2>", methods=['DELETE'])
 def remove_edge(edge):
-    return jsonify(success=TwitterGraph.elimina_conexion(str(edge)))
+    return jsonify(success=TwitterGraph.elimina_conexion(cliente,str(edge1),str(edge2)))
+
+@app.route("/vertices/cuenta_vertices", methods=['POST'])
+def count_vertex():
+    return jsonify(success=TwitterGraph.cuenta_vertices(cliente))
 
 
 
