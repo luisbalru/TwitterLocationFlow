27626f24e464f3ff2b7cbefc21ab77bbc54535aa
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-21 12:01:37 +0100

    Prueba

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -39,3 +39,4 @@ def Iniciar():
      # Iniciamos el servicio web
     with shell_env(C_URL='wss://twitterlocationflow.gremlin.cosmosdb.azure.com:443',C_USER=os.environ['CLIENT_USERNAME'], C_PASS = os.environ['CLIENT_PASSWD']):
         run('cd TwitterLocationFlow && python src/genera_credenciales.py $C_URL $C_USER $C_PASS')
+        run('sudo gunicorn app:app -b 0.0.0.0:80 &')
