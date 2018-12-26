87dd46f73ee69608b6e4ef094dc9a52d3cc432ed
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-26 13:56:43 +0100

    Adecuando a sólo ejecutar la app

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-cd ..
-vagrant up --provider=azure
-vagrant provision
-fab -f ./despliegue/fabfile.py -H vagrant@twitterlocationflow.uksouth.cloudapp.azure.com InstalarApp
+
 fab -f ./despliegue/fabfile.py -H vagrant@twitterlocationflow.uksouth.cloudapp.azure.com IniciarApp
