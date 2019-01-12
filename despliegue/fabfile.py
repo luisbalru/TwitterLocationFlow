2151777efbadd67ed51059b550e1cd5642ee89cb
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-01-12 23:36:43 +0100

    Acabando fabfile y killapp #31

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -8,7 +8,6 @@ def InstalarApp():
     run('git clone https://github.com/luisbalru/TwitterLocationFlow.git')
     with cd('TwitterLocationFlow'):
         run('pip3 install --user -r requirements.txt')
-        run('mkdir Errors')
 
 
 def ActualizarApp():
@@ -22,7 +21,8 @@ def IniciarApp():
     ActualizarApp()
     with shell_env(C_URL='wss://twitterlocationflow.gremlin.cosmosdb.azure.com:443',C_USER=os.environ['CLIENT_USERNAME'], C_PASS = os.environ['CLIENT_PASSWD']):
         run('cd TwitterLocationFlow && python src/genera_credenciales.py $C_URL $C_USER $C_PASS')
-        sudo('nohup gunicorn app:app -b 0.0.0.0:80 & echo $! > ~/pid.txt')
+        sudo('nohup gunicorn app:app -b 0.0.0.0:80 & echo $! > pid.txt')
 
 def KillApp():
-    sudo('pkill gunicorn')
+    with cd('TwitterLocationFlow')
+        sudo('var=`cat pid.txt` && kill -9 $var')
